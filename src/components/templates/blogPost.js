4c356835e8bec975113a76aa1837cefa82e22933
kimnagui <calrosban@gmail.com>
2019-07-04 16:31:46 +0900

    fix: Add category link in post & Change css in post

--- src/components/templates/blogPost.js
+++ src/components/templates/blogPost.js
@@ -1,9 +1,49 @@
 import React from "react";
 import { Link, graphql } from "gatsby";
-
+import styled from "styled-components";
+import Circle from "components/atoms/circle";
 import Layout from "./layout";
 import SEO from "../seo";
-import { rhythm, scale } from "../../utils/typography";
+
+const PostHeader = styled.div`
+    h1 {
+        padding: 0;
+        margin-bottom: 10px;
+        border: none;
+    }
+
+    div {
+        font-size: 13px;
+        color: gray;
+        margin-bottom: 10px;
+
+        a,
+        span {
+            color: inherit;
+            vertical-align: middle;
+        }
+
+        div {
+            margin: 0 10px;
+        }
+    }
+
+    hr {
+        margin-bottom: 40px;
+    }
+`;
+
+const PostContent = styled.div``;
+
+{
+    /* 
+        [Todo]
+        1. 관련글 : 현재 게시물을 기준으로 카드형식의 좌우스크롤 가능한 같은 카테고리의 게시물 리스트
+        2. 공유 : 게시글 URL 복사기능 + 공유기능
+        3. 댓글 : utterances or disqus ??
+        4. 태그
+    */
+}
 
 class BlogPost extends React.Component {
     render() {
@@ -22,23 +62,24 @@ class BlogPost extends React.Component {
                     title={post.frontmatter.title}
                     description={post.excerpt}
                 />
-                <h1>{post.frontmatter.title}</h1>
-                <p
-                    style={{
-                        ...scale(-1 / 5),
-                        display: `block`,
-                        marginBottom: rhythm(1),
-                        marginTop: rhythm(-1)
-                    }}
-                >
-                    {post.frontmatter.date}
-                </p>
-                <div dangerouslySetInnerHTML={{ __html: post.html }} />
-                <hr
-                    style={{
-                        marginBottom: rhythm(1)
-                    }}
-                />
+                <PostHeader>
+                    <h1>{post.frontmatter.title}</h1>
+
+                    <div>
+                        <Link to={`/category/${category}`}>{category}</Link>
+                        <Circle
+                            display={"inline-block"}
+                            size={"3"}
+                            color={"gray"}
+                        />
+                        <span>{post.frontmatter.date}</span>
+                    </div>
+                    <hr />
+                </PostHeader>
+                <PostContent>
+                    <div dangerouslySetInnerHTML={{ __html: post.html }} />
+                </PostContent>
+                <hr style={{ margin: "40px 0 20px 0" }} />
 
                 <ul
                     style={{
@@ -46,19 +87,40 @@ class BlogPost extends React.Component {
                         flexWrap: `wrap`,
                         justifyContent: `space-between`,
                         listStyle: `none`,
-                        padding: 0
+                        padding: 0,
+                        marginLeft: 0
                     }}
                 >
                     <li>
                         {previous && (
-                            <Link to={previous.fields.slug} rel="prev">
+                            <Link
+                                to={previous.fields.slug}
+                                rel="prev"
+                                style={{
+                                    padding: "5px 10px",
+                                    backgroundColor: "#3f0f3f",
+                                    borderRadius: "6px",
+                                    color: "#fff",
+                                    textDecoration: "none"
+                                }}
+                            >
                                 ← {previous.frontmatter.title}
                             </Link>
                         )}
                     </li>
                     <li>
                         {next && (
-                            <Link to={next.fields.slug} rel="next">
+                            <Link
+                                to={next.fields.slug}
+                                rel="next"
+                                style={{
+                                    padding: "5px 10px",
+                                    backgroundColor: "#3f0f3f",
+                                    borderRadius: "6px",
+                                    color: "#fff",
+                                    textDecoration: "none"
+                                }}
+                            >
                                 {next.frontmatter.title} →
                             </Link>
                         )}
